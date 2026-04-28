2f83ed30db2265c0c5f20322f2a03dff23930762
Fred Chasen <fchasen@mozilla.com>
2026-04-28 13:43:06 -0700

    Apply borderless button style so pointerStyle compiles on settings Links

--- Zilla/BugzillaSettingsView.swift
+++ Zilla/BugzillaSettingsView.swift
@@ -49,6 +49,7 @@ struct BugzillaSettingsView: View {
                     Label("Get a key", systemImage: "arrow.up.right.square")
                         .font(.caption)
                 }
+                .buttonStyle(.borderless)
                 .pointerStyle(.link)
             }
 
--- Zilla/PhabricatorSettingsView.swift
+++ Zilla/PhabricatorSettingsView.swift
@@ -49,6 +49,7 @@ struct PhabricatorSettingsView: View {
                     Label("Get a token", systemImage: "arrow.up.right.square")
                         .font(.caption)
                 }
+                .buttonStyle(.borderless)
                 .pointerStyle(.link)
             }
 
